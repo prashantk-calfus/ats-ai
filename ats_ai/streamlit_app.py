056cb72e14618ef11af8a7fb164e283332deec18
Prashant Kumar <prashant.kumar@calfus.com>
2025-08-04 17:16:50 +0530

    Added unsafe-allow for now

--- ats_ai/streamlit_app.py
+++ ats_ai/streamlit_app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000/")
+BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
 
 st.set_page_config(layout="wide", page_title="ATS AI")
 st.title("ATS AI : Intelligent Resume Screening")
@@ -589,18 +589,23 @@ if st.session_state.parsed_data_combined:
 
                             st.success("✅ PDF Report generated successfully!")
 
+                            # The below code snippet requires CORS settings (middleware) to be enabled on app-server
+                            # to allow direct download links to work in Streamlit.
+
                             # Inject JS to auto-download the PDF
-                            download_html = f"""
-                                <html>
-                                    <body>
-                                        <a id="download_pdf_link" href="{download_url}" download style="display:none;"></a>
-                                        <script>
-                                            document.getElementById('download_pdf_link').click();
-                                        </script>
-                                    </body>
-                                </html>
-                            """
-                            st.components.v1.html(download_html, height=0)
+                            # download_html = f"""
+                            #     <html>
+                            #         <body>
+                            #             <a id="download_pdf_link" href="{download_url}" download style="display:none;"></a>
+                            #             <script>
+                            #                 document.getElementById('download_pdf_link').click();
+                            #             </script>
+                            #         </body>
+                            #     </html>
+                            # """
+                            # st.components.v1.html(download_html, height=0)
+
+                            st.markdown(f"[📥 Click here to download your PDF report]({download_url})", unsafe_allow_html=True)
 
                         else:
                             st.error(f"❌ Failed to generate PDF: {response.text}")
